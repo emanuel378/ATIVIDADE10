2ae254e0d52aad57d60d807b6ad84c1333a37051
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-15 18:10:32 -0300

    par ou impar

---
+++ atv10.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+numero = int(input('Digite um inteiro: '))
+
+if numero%2 :
+        print("Ímpar")
+else:
+        print("Par")
